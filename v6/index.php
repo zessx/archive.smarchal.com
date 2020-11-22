60bad3f01bc3749eed1108d356e149c22823b679
zessx <contact@zes.sx>
2020-11-22 16:41:00 +0100

    :lock: HTTPS everywhere

--- v6/index.php
+++ v6/index.php
@@ -34,30 +34,30 @@
 
 		<link rel="shortcut icon" href="<?php echo asset('favicon.ico')?>" type="image/x-icon">
 
-		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="<?php echo asset('app.min.css') ?>" type='text/css'>
 	</head>
 	<body>
 		<section id="wrapper" class="wrapper closed">
 			<div class="shutter shutter-top"></div>
-			<a id="zessx" class="zessx" href="http://zes.sx">zessx</a>
+			<a id="zessx" class="zessx" href="https://zes.sx">zessx</a>
 			<div id="strip" class="strip">
 				<a href="https://www.facebook.com/marchal.samuel" class="external"><i class="icon-facebook"></i></a>
-				<a href="http://fr.viadeo.com/fr/profile/samuel.marchal2" class="external"><i class="icon-viadeo"></i></a>
-				<a href="http://www.linkedin.com/pub/samuel-marchal/26/a73/584" class="external"><i class="icon-linkedin"></i></a>
-				<a href="http://careers.stackoverflow.com/zessx" class="external"><i class="icon-careers"></i></a>
+				<a href="https://fr.viadeo.com/fr/profile/samuel.marchal2" class="external"><i class="icon-viadeo"></i></a>
+				<a href="https://www.linkedin.com/pub/samuel-marchal/26/a73/584" class="external"><i class="icon-linkedin"></i></a>
+				<a href="https://careers.stackoverflow.com/zessx" class="external"><i class="icon-careers"></i></a>
 				<a href="mailto:contact@zes.sx"><i class="icon-email"></i></a>
 				<span class="separator"></span>
-				<a href="http://blog.smarchal.com" class="external"><i class="icon-rss"></i></a>
+				<a href="https://blog.smarchal.com" class="external"><i class="icon-rss"></i></a>
 				<a href="https://google.com/+SamuelMarchal-zessx" class="external"><i class="icon-gplus"></i></a>
 				<a href="https://www.twitter.com/zessx" class="external"><i class="icon-twitter"></i></a>
 				<a href="https://github.com/zessx" class="external"><i class="icon-github"></i></a>
-				<a href="http://stackoverflow.com/users/1238019/zessx" class="external"><i class="icon-stackoverflow"></i></a>
+				<a href="https://stackoverflow.com/users/1238019/zessx" class="external"><i class="icon-stackoverflow"></i></a>
 			</div>
 			<div class="shutter shutter-bottom">
 				<p>
 					<strong>Samuel Marchal</strong><small> - zessx</small><br>
-					Web Developer - <a href="http://www.alancia.fr/" class="external">Alancia</a>
+					Web Developer - <a href="https://www.alancia.fr/" class="external">Alancia</a>
 				</p>
 			</div>
 		</section>
